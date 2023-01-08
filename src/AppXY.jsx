4d2e82bf3772b3525749aabccf429bbb3716fbdf
Bella <shdudtnr3939@naver.com>
2023-01-08 14:31:29 +0900

    [modified] 마우스 따라 가기 - 엘리님 구현

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,17 +2,28 @@ import React, { useState } from "react";
 import "./AppXY.css";
 
 export default function AppXY() {
-  const [xy, SetXY] = useState({ x: 0, y: 0 });
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const [xy, setXY] = useState({ x: 0, y: 0 });
 
   const handleMouseMove = (e) => {
-    SetXY({ x: e.clientX, y: e.clientY });
+    setXY({ x: e.clientX, y: e.clientY });
   };
 
   return (
-    <div className="container" onMouseMove={(e) => handleMouseMove(e)}>
+    <div
+      className="container"
+      onPointerMove={(e) => {
+        console.log(e);
+        setX(e.clientX);
+        setY(e.clientY);
+        // handleMouseMove(e);
+      }}>
       <div
         className="pointer"
-        style={{ position: "absolute", left: xy.x, top: xy.y }}
+        style={{ transform: `translate(${x}px, ${y}px)` }}
+        // style={{ position: "absolute", left: xy.x, top: xy.y }}
       />
     </div>
   );
