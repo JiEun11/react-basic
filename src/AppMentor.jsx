4729269e43ad9bda02c6c0971f171b0383bc3ddd
Bella <shdudtnr3939@naver.com>
2023-01-08 19:14:58 +0900

    [modified] 중첩 객체 상태 관리 (멘토바꾸기) - 내 답

---
+++ src/AppMentor.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const AppMentor = () => {
+  const [person, setPerson] = useState({
+    name: "벨라",
+    title: "개발자",
+    mentor: {
+      name: "엘리",
+      title: "시니어개발자",
+    },
+  });
+
+  const handleMentorName = (changedName) => {
+    setPerson({ ...person, mentor: { name: changedName } });
+  };
+  return (
+    <div>
+      <h1>
+        {person.name}는 {person.title}
+      </h1>
+      <p>
+        {person.name}의 멘토는 {person.mentor.name} ({person.mentor.title})
+      </p>
+      <button
+        onClick={() => {
+          const name = prompt(`what's yoru mentor's name?`);
+          setPerson((prev) => ({
+            ...prev,
+            mentor: { name: name, title: prev.mentor.title },
+          }));
+        }}>
+        멘토 이름 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          const title = prompt(`what's your mentor's title?`);
+          setPerson((prev) => ({
+            ...prev,
+            mentor: { name: prev.mentor.name, title: title },
+          }));
+        }}>
+        멘토 타이틀 바꾸기
+      </button>
+    </div>
+  );
+};
+
+export default AppMentor;
