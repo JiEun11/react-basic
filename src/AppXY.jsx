4d7f7c491e78ced1f040844fe4352e6b026536d4
Bella <shdudtnr3939@naver.com>
2023-01-08 18:25:56 +0900

    [modified] 마우스 따라가기 완료

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -17,6 +17,8 @@ export default function AppXY() {
         console.log(e);
         // handleMouseMove(e);
         setPosition({ x: e.clientX, y: e.clientY });
+        // 만약 수평으로만 이동이 가능하다면?
+        // setPosition((prev) => ({ ...prev, x: e.clientX }));
       }}>
       <div
         className="pointer"
