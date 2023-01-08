f4f032139140d852cb61492a19977e056be1758b
Bella <shdudtnr3939@naver.com>
2023-01-08 14:19:31 +0900

    [add] 마우스 따라가기 - 내 구현

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import './AppXY.css';
+import React, { useState } from "react";
+import "./AppXY.css";
 
 export default function AppXY() {
+  const [xy, SetXY] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    SetXY({ x: e.clientX, y: e.clientY });
+  };
+
   return (
-    <div className='container'>
-      <div className='pointer' />
+    <div className="container" onMouseMove={(e) => handleMouseMove(e)}>
+      <div
+        className="pointer"
+        style={{ position: "absolute", left: xy.x, top: xy.y }}
+      />
     </div>
   );
 }
