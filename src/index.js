b86017b2eecb49bf2a6b7620fb18713bd80ca7a9
Bella <shdudtnr3939@naver.com>
2023-01-29 18:45:35 +0900

    [add] Form을 만드는 법-uncontrolled component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import AppMentors from "./AppMentors";
 import AppMentorsImmer from "./AppMentorsImmer";
+import AppForm from "./AppForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    {/* <AppMentors /> */}
-    <AppMentorsImmer />
+    <AppForm />
   </>
 );
 
