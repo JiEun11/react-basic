05d3921d38ae24bc5638c55badcf902cdacc4ede
Bella <shdudtnr3939@naver.com>
2023-02-19 22:43:13 +0900

    [add] 커스텀훅 만들기

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import useProducts from "../../hooks/use-products";
 
 export default function Products() {
   // const [count, setCount] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-  const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
+  const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked((prev) => !prev);
-
-  useEffect(() => {
-    setLoading(true);
-    setError(undefined);
-    fetch(`data/${checked ? "sale_" : ""}products.json`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("🔥뜨끈한 데이터를 네트워크에서 받아옴");
-        setProducts(data);
-      })
-      .catch((e) => setError("에러 발생!"))
-      .finally(() => setLoading(false));
-    return () => {
-      console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
-    };
-  }, [checked]);
 
   if (loading) return <p>Loading...</p>;
 
@@ -33,7 +16,8 @@ export default function Products() {
         id="checkbox"
         type="checkbox"
         value={checked}
-        onChange={handleChange}></input>
+        onChange={handleChange}
+      />
       <label htmlFor="checkbox">Show Only 🔥 Sale</label>
       <ul>
         {products.map((product) => (
